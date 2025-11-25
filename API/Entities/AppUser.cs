68d35c47903e8446c870ea12c8d0c343d1ece10b
Hedayat Hoshmand <hedayatullah.hoshmand@gmail.com>
2025-11-25 13:10:08 -0800

    End of Section two make git repository and add chagned commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+
+namespace API.Entities;
+
+public class AppUser
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+}
